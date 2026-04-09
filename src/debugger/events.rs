743ae0a758582c79f92f9297d0ca2ba8560704cb
0xVector <hello@0xvector.me>
2026-04-09 17:51:53 +0200

    debugger: Send Terminated DAP event along with Exited when MSIM exits

--- src/debugger/events.rs
+++ src/debugger/events.rs
@@ -1,6 +1,7 @@
 use super::{Debugger, Result};
 use crate::Address;
 use crate::target::DebugTarget;
+use dap::base_message::Sendable;
 
 type EventResult = Result<Option<dap::events::Event>>;
 
@@ -12,7 +13,11 @@ type EventResult = Result<Option<dap::events::Event>>;
     clippy::needless_pass_by_ref_mut
 )]
 impl<T: DebugTarget> Debugger<T> {
-    pub(super) const fn handle_event_exited(&mut self) -> EventResult {
+    pub(super) fn handle_event_exited(&mut self) -> EventResult {
+        // TODO: we could allow returning multiple events (like in request handlers)
+        self.dap_session
+            .send(Sendable::Event(dap::events::Event::Terminated(None)))?;
+
         Ok(Some(dap::events::Event::Exited(
             dap::events::ExitedEventBody { exit_code: 0 }, // TODO: track exit code?
         )))

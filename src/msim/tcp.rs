13068fd7097e7db5d5ef95e042b392e6a80eaf55
0xVector <hello@0xvector.me>
2026-04-09 11:26:36 +0200

    msim: Fix read timeout in TCP socket

--- src/msim/tcp.rs
+++ src/msim/tcp.rs
@@ -6,7 +6,8 @@ use std::time::Duration;
 pub fn connect(port: u16) -> Result<TcpStream> {
     let s = TcpStream::connect(("127.0.0.1", port))?;
     s.set_nodelay(true)?;
-    s.set_read_timeout(Some(Duration::from_secs(5)))?;
+    // Has to be disabled so background listener doesn't get killed
+    s.set_read_timeout(None)?;
     s.set_write_timeout(Some(Duration::from_secs(5)))?;
     eprintln!("Connected to MSIM with port {port}");
     Ok(s)
